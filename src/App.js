fd808d32965d99b06b0cdfa07ed0d4264d117596
Troy Dorman <troydorman@gmail.com>
2021-09-27 08:36:24 -0600

    wrapped our app in our provider and imported our consumer into tracks.  displaying passed data from the context state into tracks

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
 
+import { Provider } from "./context";
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <Provider>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </Provider>
   );
 }
 
